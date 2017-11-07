bf7ae2e0874a472d07e43cde356ce8f48ecdd314
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-07 15:07:13 -0500

    Fix activate/deactivate Windows install location
    Was installing the activate/deactivate scripts into `%LIBRARY_PREFIX%`
    on Windows. However these scripts should be installed in `%PREFIX%`.
    Hence this updates the install location on Windows of the
    activate/deactivate scripts.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 setlocal EnableDelayedExpansion
 
-:: Copy the [de]activate scripts to %LIBRARY_PREFIX%\etc\conda\[de]activate.d.
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 :: This will allow them to be run on environment activation.
 FOR %%F IN (activate deactivate) DO (
-    IF NOT EXIST %LIBRARY_PREFIX%\etc\conda\%%F.d MKDIR %LIBRARY_PREFIX%\etc\conda\%%F.d
-    COPY %RECIPE_DIR%\%%F.bat %LIBRARY_PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    IF NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
+    COPY %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
 )
